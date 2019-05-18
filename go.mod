fcd640a67b6ef9dca1713327744978d08bf5414a
noituri <yknomeh@protonmail.com>
2019-05-18 14:59:29 +0200

    tui: changed tui lib & added image in tui

--- go.mod
+++ go.mod
@@ -4,11 +4,14 @@ go 1.12
 
 require (
 	github.com/cloudfoundry/gosigar v1.1.0
-	github.com/gdamore/tcell v1.1.2
+	github.com/disintegration/imaging v1.6.0
+	github.com/gizak/termui/v3 v3.0.0
+	github.com/maruel/panicparse v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.4 // indirect
+	github.com/mitchellh/go-wordwrap v1.0.0 // indirect
 	github.com/onsi/ginkgo v1.8.0
 	github.com/onsi/gomega v1.5.0
 	github.com/pkg/errors v0.8.1
-	github.com/rivo/tview v0.0.0-20190515161233-bd836ef13b4b
 	github.com/stretchr/testify v1.3.0
 	golang.org/x/sys v0.0.0-20190516110030-61b9204099cb
 )
--- tui.go
+++ tui.go
@@ -1,30 +1,68 @@
 package main
 
 import (
-	"github.com/rivo/tview"
+	"encoding/base64"
+	"github.com/disintegration/imaging"
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"strconv"
+	"strings"
 )
 
-func GetHomePage() *tview.Grid {
-	testText := tview.NewTextView().SetTextAlign(tview.AlignCenter).SetText("zfetch")
+const TEST_LOGO = `iVBORw0KGgoAAAANSUhEUgAAAFAAAABNCAMAAAAGhxPaAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAhFBMVEU1v1wAAAA1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1w1v1wAAACLICWOAAAAKnRSTlMAAOAf/Ozp6evNHOVDKCkqJAUCEhUUGrrW1O3v0B0uLykGEREPAs/Qthmsk4gjAAAAAWJLR0QB/wIt3gAAAAd0SU1FB+MFEQgvL8ufKIoAAACWSURBVFjD7dNZCsJAEEXRMnGeTWIS53nc/wJ1AdWCT9AI9/4WfaCg2u7vF8VmcRQYGiAgICAgICDgx2C90WwFane6AtjrD4aBRuOJAD7Xqr2oGqAlaTZ1ytJEW9nyopw5lUUugqFH0tkA/hKcuy2WKxVcu222OxUUvteXwb3b4XhSVz67Xa63ypwNICAgICAgICDgP4MPIjViHiX1RUQAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTktMDUtMTdUMDg6NDc6NDctMDQ6MDCAF1wrAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE5LTA1LTE3VDA4OjQ3OjQ3LTA0OjAw8UrklwAAAABJRU5ErkJggg==`
+func GetMainGrid() *ui.Grid {
+	grid := ui.NewGrid()
+	termWidth, termHeight := ui.TerminalDimensions()
+	grid.SetRect(0, 0, termWidth, termHeight)
 
-	return tview.NewGrid().
-		SetRows(-1).
-		SetColumns(5).
-		SetBorders(true).
-		AddItem(testText, 0, 0, 1, 3, 0, 0, true)
+	imgLogo, _, err := image.Decode(base64.NewDecoder(base64.StdEncoding, strings.NewReader(TEST_LOGO)))
+	imgLogo = imaging.Resize(imgLogo, termWidth / 5, termWidth / 10, imaging.Lanczos)
+
+	if err != nil {
+		log.Fatalf("Could not load the image " + err.Error())
+	}
+
+	imgWidget := widgets.NewImage(imgLogo)
+	imgWidget.SetRect(0, 0, 5, 5)
+
+
+	p := widgets.NewParagraph()
+	p.Text = strconv.FormatInt(int64(termWidth), 10)
+	p.SetRect(0, 0, 25, 5)
+	p.PaddingLeft = termWidth / 2
+
+	grid.Set(
+		ui.NewRow(1.0,
+			ui.NewCol(1.0, imgWidget),
+		),
+	)
+
+	return grid
 }
 
 func StartTui() {
-	app := tview.NewApplication()
+	if err := ui.Init(); err != nil {
+		log.Fatalf("failed to initialize termui: %v", err)
+	}
 
-	//box := tview.NewBox().SetBorder(true).SetBorderColor(tcell.Color45)
-	layout := tview.NewFlex().SetDirection(tview.FlexRow).SetFullScreen(true)
-	//layout.AddItem(box, 0, 1, false)
-	layout.AddItem(GetHomePage(), 0, 1, true)
+	defer ui.Close()
 
-	app.SetRoot(layout, true)
+	ui.Render(GetMainGrid())
 
-	if err := app.Run(); err != nil {
-		panic(err)
+	for {
+		select {
+		case e := <-ui.PollEvents():
+			switch e.ID {
+			case "q", "<C-c>":
+				return
+			case "<Resize>":
+				ui.Clear()
+				ui.Render(GetMainGrid())
+			}
+		}
 	}
 }
